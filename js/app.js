c82f2ea4c76eee54b8223275be78d1a4c231a32c
Will Culpepper <willculpepper@icloud.com>
2022-08-16 00:25:00 -0600

    added some stuff to the portfolio, started a draft of a WebGL app for the top section.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
 $(document).foundation()
+
+function main(){
+    const canvas = document.querySelector("#glCanvas");
+    const gl = canvas.getContext("webgl");
+
+    if(gl == null){
+        alert("Unable to initialize webGL app. Your browser might not support it.");
+        return;
+    }
+
+    gl.clearColor(0.0,0.0,0.0,1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+}
+
+window.onload = main;
